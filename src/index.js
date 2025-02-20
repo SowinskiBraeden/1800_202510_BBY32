0ff834f4d972f48a9246a27d084648fadf0f3bee
SowinskiBraeden <braeden@sowinski.dev>
2025-02-20 11:14:32 -0800

    update file structure + 404 page

--- src/index.js
+++ src/index.js
@@ -7,7 +7,13 @@ const port = 8000;
 app.use('/static', express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.status(200);
+  return res.sendFile(path.join(__dirname, '/views/index.html'));
+});
+
+app.get("*", (req, res) => {
+  res.status(404);
+  return res.sendFile(path.join(__dirname, '/views/notfound.html'));
 });
 
 app.listen(port, () => {
