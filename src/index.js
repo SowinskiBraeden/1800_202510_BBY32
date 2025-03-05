bd14b9a0b09d4517404127f8902c0dfae7169897
Kolvin <carifianto@my.bcit.ca>
2025-03-05 12:33:22 -0800

    adding empty fav page

--- src/index.js
+++ src/index.js
@@ -21,9 +21,19 @@ app.get("/dashboard", (req, res) => {
   return res.sendFile(path.join(__dirname, '/views/dashboard.html'));
 });
 
+app.get("/dashboard/favourites", (req, res) => {
+  res.status(200);
+  return res.sendFile(path.join(__dirname, '/views/favourites.html'));
+});
+
 app.get("/dashboard/profile", (req, res) => {
   res.status(200);
   return res.sendFile(path.join(__dirname, '/views/profile.html'));
+});
+
+app.get("/dashboard/listing", (req, res) => {
+  res.status(200);
+  return res.sendFile(path.join(__dirname, '/views/listing.html'));
 });
 
 app.get("*", (req, res) => {
